b84eba716b959dbbba8af7311888809441312913
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2022-01-26 00:50:51 +0900

    use Hook to write code more simply

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:mvvm_sample/provider/counter_provider.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends HookWidget {
   const HomeView({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
   Widget build(BuildContext context) {
+    final _count = useProvider(homeViewModelProvider);
+
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
@@ -24,21 +27,17 @@ class HomeView extends StatelessWidget {
               const Text(
                 'You have pushed the button this many times:',
               ),
-              Consumer(
-                builder: (context, watch, child) => Text(
-                  watch(homeViewModelProvider).toString(),
+              Text(
+                  _count.toString(),
                   style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
             ],
           ),
         ),
-        floatingActionButton: Consumer(
-          builder: (context, watch, child) => FloatingActionButton(
-            onPressed: () => context.read(homeViewModelProvider.notifier).incrementCounter(),
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => context.read(homeViewModelProvider.notifier).incrementCounter(),
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
         ),
     );
   }
